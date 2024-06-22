1500fc5bdc993e46d33e1e4d08231261f59ca388
Rajesh <156993811+CodeWithRajMishra@users.noreply.github.com>
2024-06-22 18:27:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express= require("express");
 const cors= require("cors");
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://combineproject.vercel.app'
+}));
+
 app.get("/api", (req, res)=>{
 
     try {
